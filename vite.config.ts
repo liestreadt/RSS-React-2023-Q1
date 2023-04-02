7ad0f2df1ccabea3c3b3564b2d4e69a1c19477d3
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-04-03 01:07:20 +0300

    chore: add test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -11,5 +11,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      reporter: ['text'],
+      all: true,
+    },
   },
 });

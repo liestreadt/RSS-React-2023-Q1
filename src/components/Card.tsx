5ccb5a3d2135dee094baf83b5de9a76c0f218a1a
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-04-08 00:59:35 +0300

    feat: change cards content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../styles/Card.css';
 
-import { getDateFromResponse } from '../helpers/helperFunctions';
-import { Article } from '../helpers/types';
+import { Character } from '../helpers/types';
 
-export function Card(props: { article: Article }): JSX.Element {
+export function Card(props: { article: Character }): JSX.Element {
+  function handleCardClick(): void {
+    console.log(props.article.name);
+  }
+
   return (
     <div className="card">
-      <div className="card__title">{props.article.title}</div>
-      <div className="card__author">Author: {props.article.source.name}</div>
-      <div className="card__publish">{getDateFromResponse(props.article.publishedAt)}</div>
-      <div className="card__content">{props.article.content}</div>
-      <img className="card__image" src={props.article.image} alt="image" />
+      <div className="card__title">{props.article.name}</div>
+      <img
+        onClick={handleCardClick}
+        className="card__image"
+        src={props.article.image}
+        alt="image"
+      />
     </div>
   );
 }
--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,3 +1,18 @@
+export type Character = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: string;
+  location: string;
+  image: string;
+  episode: string;
+  url: string;
+  created: string;
+};
+
 export type Article = {
   description: string;
   content: string;

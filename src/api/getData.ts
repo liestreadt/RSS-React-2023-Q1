ed05c00b18eb0f473b6b4a1f3f343e1bc475f7de
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-04-10 00:47:21 +0300

    feat: add request for specific character

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -12,3 +12,16 @@ export async function getData(searchInput: string): Promise<Character[]> {
     return [];
   }
 }
+
+export async function getCurrentCharacter(id: number): Promise<Character | null> {
+  try {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`, {
+      method: 'GET',
+    });
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.warn(e);
+    return null;
+  }
+}

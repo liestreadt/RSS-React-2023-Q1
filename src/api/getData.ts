5ab05affa5be8adb88008fb30edf7de6a8682e98
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-04-10 21:58:45 +0300

    fix: fix request functions

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,27 +1,24 @@
 import { Character } from '../helpers/types';
 
-export async function getData(searchInput: string): Promise<Character[]> {
+export async function getData(searchInput: string): Promise<Character[] | null> {
   try {
     const response = await fetch(`https://rickandmortyapi.com/api/character?name=${searchInput}`, {
       method: 'GET',
     });
+    if (!response.ok) {
+      throw new Error('getData response is not ok');
+    }
     const result = await response.json();
     return result.results;
   } catch (e) {
-    console.warn(e);
-    return [];
+    return null;
   }
 }
 
-export async function getCurrentCharacter(id: number): Promise<Character | null> {
-  try {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`, {
-      method: 'GET',
-    });
-    const result = await response.json();
-    return result;
-  } catch (e) {
-    console.warn(e);
-    return null;
-  }
+export async function getCurrentCharacter(id: number): Promise<Character> {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`, {
+    method: 'GET',
+  });
+  const result = await response.json();
+  return result;
 }

08febe7025f7f668899c9db7e6babfdb5920e603
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-04-17 00:21:18 +0300

    feat: implement RTK to main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { changeSearchResults, changeSearchInput } from '../redux/reduxSlices/searchSlice';
+import { useGetCharactersQuery } from '../redux/reduxAPI';
 import '../styles/Main.css';
 
-import { getData } from '../api/getData';
 import { Character } from '../helpers/types';
 import { Search } from '../components/Search';
 import { Card } from '../components/Card';
@@ -9,27 +11,21 @@ import { Modal } from '../components/Modal';
 import { Progressing } from '../components/Progressing';
 
 export function Main(): JSX.Element {
-  const inputValueInLocalStorage = localStorage.getItem('inputValue');
-  const [articles, setArticles] = useState<Character[] | null>(null);
+  const inputValueInStore = useAppSelector((state) => state.search.searchInput);
+  const dispatch = useAppDispatch();
   const [currentCharacter, setCurrentCharacter] = useState<Character | null>(null);
-  const [searchInput, setSearchInput] = useState<string>(inputValueInLocalStorage || '');
   const [isModalActive, setIsModalActive] = useState<boolean>(false);
-  const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  const { isLoading, isError, data } = useGetCharactersQuery(inputValueInStore);
 
   useEffect(() => {
-    setIsFetching(true);
-    getData(searchInput).then((data) => {
-      setArticles(data);
-      setIsFetching(false);
-      console.log(data);
-    });
-  }, [searchInput]);
+    dispatch(changeSearchResults(data));
+  }, [data, dispatch]);
 
   function handleSearchSubmitOnKey(event: React.KeyboardEvent) {
     const input = event.target as HTMLInputElement;
     if (event.key === 'Enter') {
-      setSearchInput(input.value);
-      localStorage.setItem('inputValue', input.value);
+      dispatch(changeSearchInput(input.value));
     }
   }
 
@@ -42,23 +38,23 @@ export function Main(): JSX.Element {
         setActive={setIsModalActive}
       />
       <h1 className="h1">Rick and Morty API</h1>
-      <Search setSearchInput={setSearchInput} onSearchSubmit={handleSearchSubmitOnKey} />
+      <Search onSearchSubmit={handleSearchSubmitOnKey} />
       <div className="main">
-        {isFetching ? (
+        {isLoading ? (
           <Progressing />
-        ) : (
-          articles?.map((article: Character, index) => (
+        ) : !isError ? (
+          data?.map((article: Character, index) => (
             <Card
               setCurrentCharacter={setCurrentCharacter}
               setActive={setIsModalActive}
               key={index}
               article={article}
             />
-          )) || (
-            <div>
-              <b>No results found</b>
-            </div>
-          )
+          ))
+        ) : (
+          <div>
+            <b>No results found</b>
+          </div>
         )}
       </div>
     </>

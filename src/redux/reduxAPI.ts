438163f84422ba51b8a3dcd5fa7085480dfc5e57
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-04-17 22:58:31 +0300

    fix: import isomorphic-fetch to reduxApi.ts

--- src/redux/reduxAPI.ts
+++ src/redux/reduxAPI.ts
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import fetch from 'isomorphic-fetch';
 import type { Character, ResponseOnNameFilter } from '../helpers/types';
 
 export const charactersApi = createApi({
   reducerPath: 'charactersApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api/', fetchFn: fetch }),
   endpoints: (build) => ({
     getCharacters: build.query<Character[], string>({
       query: (name: string) => `character?name=${name}`,

998fb4d37f3b0e69d60284d7431bc2d1ae7c3a8d
Vlad_nikishin <Nik_vladislav@outlook.com>
2023-04-30 23:08:48 +0300

    fix: fix lint errors

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 import './styles/index.css';
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
-import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 import './styles/index.css';
-import { Main } from './pages/Main';
 
 type RenderProps = {
   path: string;
